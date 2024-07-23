5a93cb013026bcc7a0289c1b8998252a8fdda866
pbrightfarms <142038195+pbrightfarms@users.noreply.github.com>
2024-07-23 10:09:24 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_config_lib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    description='A library for configuration settings',
+    author='Prince Raghuvanshi',
+    author_email='praghuvanshi@brightfarms.com',
+    url='https://github.com/nitprince1907/my_config_lib',
+)
